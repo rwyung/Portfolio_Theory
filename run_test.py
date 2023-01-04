b161895b4c92e14204744ee307422b1e99eac1fc
rikky00 <maceswinger23@gmail.com>
2023-01-04 00:21:53 -0500

    added additional test for non amortizing swap

--- run_test.py
+++ run_test.py
@@ -2,7 +2,7 @@
 import scipy
 import numpy as np
 from CurveObj import CurveObj
-from bondutils import PVCF, CustomCashflowPV
+from bondutils import PVCF, CustomCashflowPV, non_amortizing_swap_val
 import matplotlib.pyplot as plt
 terms_list = [1/365, 5/365, 10/365,1/12,3/12,6/12,9/12,1.,3.,5.,7.,10.,15.,20.,25.,30.]
 rates_list = [0.025, 0.0252,0.02535,0.0255,0.0259, 0.0295, 0.0315,0.0334,0.0318,0.0308, 0.0305,0.0304,0.031, 0.0314,0.0305,0.0301]
@@ -51,3 +51,11 @@ term_array = np.arange(0,10+0.5,0.5)
 test_8 = CustomCashflowPV(term_array,Cashflows=Cashflow,df=r_1, freq=freq)
 print("Cashflow Amount_test7 is: {}".format(test_7))
 print("Test 8 Results: ",test_8)
+
+
+Principal = 10e6
+#r_2 = [0.07 for _ in range(len(term_array))]
+r_2 = np.random.normal(0.05, 0.0025, len(term_array))
+df = [0.03 for _ in range(len(term_array))] # indexed to a seperate risk free rat
+test_9 = non_amortizing_swap_val(Principal, term_array, r_1,r_2,df,freq=2)
+print(f"Swap was valued at {test_9:0.2f}")
