1865b4d42a6f2ed805c8f51b5991e8fb6772b76a
rikky00 <maceswinger23@gmail.com>
2023-01-05 12:15:38 -0500

    "added test_unit.py package to help in unit tests"

---
+++ test_unit.py
@@ -0,0 +1,21 @@
+# test_unit.py
+
+from math import abs
+def test_equal(name,f,d):
+    if f == d:
+        return(f"Unit test {name} Passed")
+    else:
+        return(f"Unit test {name} Failed")
+
+def test_abs_tol(name,f,d,tol):
+    if abs(f - d) <= tol:
+        return(f"Unit test {name} Passed; within {tol:0.05f}")
+    else:
+        return(f"Unit test {name} Failed; not within {tol:0.05f}")
+
+def test_rel_tol(name,f,d,tol):
+    if abs(f/d -1) <= tol:
+        return(f"Unit test {name} Passed; within {tol:0.05f}")
+    else:
+        return(f"Unit test {name} Failed; not within {tol:0.05f}")
+
